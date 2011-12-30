f4bff53ae369c5adaf80ba2e8e6606c0b63b0712
Tim Caswell <tim@creationix.com>
2011-12-30 16:19:54 -0600

    Clean up cachebatch

--- cachebatch.js
+++ cachebatch.js
@@ -1,39 +1,43 @@
 var FS = require('fs');
 
-function onRead(err, file) {
-  if (err) throw err;
-}
-
+// A combined wrapper for both caching and batching of requests
 var requestBatches = {};
 var requestCache = {};
 function readFile(filename, callback) {
+  // First check for a cache hit
   if (requestCache.hasOwnProperty(filename)) {
-    callback(null, requestCache[filename]);
+    var value = requestCache[filename];
+    // Delay result till next tick to act async
+    process.nextTick(function () {
+      callback(null, value);
+    });
     return;
   }
+  // Otherwise check to see if there is already a batch
   if (requestBatches.hasOwnProperty(filename)) {
     requestBatches[filename].push(callback);
     return;
   }
+
+  // If there is neighter, create a new batch and request
   var batch = requestBatches[filename] = [callback];
-  FS.readFile(filename, 'utf8', function (err, contents) {
-    if (!err) {
-      requestCache[filename] = contents;
-    }
+  FS.readFile(filename, 'utf8', onRealRead);
+
+  // Cache the result if there is no error and flush the batch
+  function onRealRead(err, contents) {
+    if (!err) requestCache[filename] = contents;
     delete requestBatches[filename];
     for (var i = 0, l = batch.length; i < l; i++) {
       batch[i].apply(null, arguments);
     }
-  });
+  }
 }
 
-// Request the same resource 10,000 times concurrently 10,000 times serially
+// Request the same resource 10,000 times using 100 parallel chains
 var left = 10000;
-(function next(err) {
-  if (err) throw err;
-  if (left) return;
-  for (var i = 0; i < 10000; i++) {
-    readFile(__filename, onRead);
-  }
-  left--;
-})();
+for (var i = 0; i < 100; i++) {
+  (function next() {
+    if (left-- <= 0) return;
+    readFile(__filename, next);
+  }());
+}
