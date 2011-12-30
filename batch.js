382b61e1ec38ca4a28d7f40111a7f9a33a162ebd
Tim Caswell <tim@creationix.com>
2011-12-30 14:14:36 -0600

    Tighten up stack.js

--- batch.js
+++ batch.js
@@ -4,31 +4,24 @@ function onRead(err, file) {
   if (err) throw err;
 }
 
-// A simple hash object to store the batches
 var requestBatches = {};
 function batchingReadFile(filename, callback) {
 
-  // Check if it's already in progress
   if (requestBatches.hasOwnProperty(filename)) {
-    // Join the existing batch and return
     requestBatches[filename].push(callback);
     return;
   }
 
-  // There is no existing batch, so start a new one
   var batch = requestBatches[filename] = [callback];
 
-  // Kick off the real request
-  FS.readFile(filename, 'utf8', function (err, contents) {
-
-    // Since we're done, we need to make sure new requests get their own batch
+  FS.readFile(filename, 'utf8', onRead);
+  
+  function onRead(err, contents) {
     delete requestBatches[filename];
-
-    // Tell all interested parties the result
     for (var i = 0, l = batch.length; i < l; i++) {
       batch[i].apply(null, arguments);
     }
-  });
+  }
 }
 
 for (var i = 0; i < 10000; i++) {
