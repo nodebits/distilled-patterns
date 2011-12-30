3d01d7826b7dac427dab676f1aef68b48f64001f
Tim Caswell <tim@creationix.com>
2011-12-30 15:52:37 -0600

    Reformat a little

--- batch.js
+++ batch.js
@@ -1,22 +1,20 @@
 var FS = require('fs');
 
-function onRead(err, file) {
-  if (err) throw err;
-}
-
+// Batching wrapper around the real FS.readFile
 var requestBatches = {};
 function batchingReadFile(filename, callback) {
-
+  // First check to see if there is already a batch
   if (requestBatches.hasOwnProperty(filename)) {
     requestBatches[filename].push(callback);
     return;
   }
 
+  // Otherwise start a new one and make a real request
   var batch = requestBatches[filename] = [callback];
-
-  FS.readFile(filename, 'utf8', onRead);
+  FS.readFile(filename, 'utf8', onRealRead);
   
-  function onRead() {
+  // Flush out the batch on complete
+  function onRealRead() {
     delete requestBatches[filename];
     for (var i = 0, l = batch.length; i < l; i++) {
       batch[i].apply(null, arguments);
@@ -24,6 +22,12 @@ function batchingReadFile(filename, callback) {
   }
 }
 
+// Request the same resource 10,000 times at once
 for (var i = 0; i < 10000; i++) {
   batchingReadFile(__filename, onRead);
 }
+
+function onRead(err, file) {
+  if (err) throw err;
+}
+
--- cache.js
+++ cache.js
@@ -1,36 +1,31 @@
 var FS = require('fs');
 
-function onRead(err, file) {
-  if (err) throw err;
-}
-
-// A simple hash object to store the cached results
+// Caching wrapper around the real FS.readFile
 var requestCache = {};
 function cachingReadFile(filename, callback) {
-
-  // Check if this request is in the cache
+  // First check for a cache hit
   if (requestCache.hasOwnProperty(filename)) {
-    // If it is, return the value and we're done!
-    callback(null, requestCache[filename]);
+    var value = requestCache[filename];
+    // Delay result till next tick to act async
+    process.nextTick(function () {
+      callback(null, value);
+    });
     return;
   }
 
-  // Otherwise, start a real request
-  FS.readFile(filename, 'utf8', function (err, contents) {
-    // If the result wasn't an error, remember the result
-    if (!err) {
-      requestCache[filename] = contents;
-    }
-    // Forward the response (good or bad) to the caller.
+  // Otherwise start a real request
+  FS.readFile(filename, 'utf8', onRealRead);
+
+  // Cache the result if there is no error
+  function onRealRead(err, contents) {
+    if (!err) requestCache[filename] = contents;
     callback(err, contents);
-  });
+  }
 }
 
 // Request the same resource 10,000 times serially
 var left = 10000;
-(function next(err) {
-  if (err) throw err;
-  if (left) return;
+(function next() {
+  if (!(left--)) return;
   cachingReadFile(__filename, next);
-  left--;
-})();
+}());
